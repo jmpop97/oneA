1725a5301c6edce83b17c98aebe8f543b109ef97
jmpop <pjm970128@gmail.com>
2023-03-14 05:20:13 +0900

    change place
    1.css,image폴더 제작
    2.연결

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://oneA:<password>@onea.ojn8ull.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
+# 메인 페이지
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
