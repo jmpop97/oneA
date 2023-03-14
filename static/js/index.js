5dfd1c75798d7e130bdc3bb7e290e4befec45681
Yun Chanhyo <ckschan95@gmail.com>
2023-03-14 11:21:56 +0900

    changed html, css

--- static/js/index.js
+++ static/js/index.js
@@ -1,49 +1,10 @@
 function save_member() {
-    let m_name= "이름"
-    let m_mbti="mbti"
-    let m_role="역할"
-    let m_address="주소"
-    let m_comment="소개"
+    let a= "저장"
 
     let formData = new FormData();
-    formData.append("m_name_give",m_name);
-    formData.append("m_mbti_give",m_mbti);
-    formData.append("m_role_give",m_role);
-    formData.append("m_address_give",m_address);
-    formData.append("m_comment_give",m_comment);
+    formData.append("a_give",a);
     alert('hi')
-    fetch('/create_member', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
+    fetch('/save_member', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
         alert(data['msg']);
     })
 }
-function update_member() {
-    let m_id="640fbdcbae352a14a1ee125a"
-    let m_name= "이름수정됨"
-    let m_mbti="mbti수정됨"
-    let m_role="역할수정됨"
-    let m_address="주소수정됨"
-    let m_comment="소개수정됨"
-
-    let formData = new FormData();
-    formData.append("m_id_give",m_id);
-    formData.append("m_name_give",m_name);
-    formData.append("m_mbti_give",m_mbti);
-    formData.append("m_role_give",m_role);
-    formData.append("m_address_give",m_address);
-    formData.append("m_comment_give",m_comment);
-    alert('fix')
-    fetch('/update_member', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
-        alert(data['msg']);
-    })
-}
-function delete_member() {
-    let m_id="640fbdcbae352a14a1ee125a"
-    
-    let formData = new FormData();
-    formData.append("m_id_give",m_id);
-    alert('삭제')
-    fetch('/delete_member', { method: "DELETE", body: formData }).then((res) => res.json()).then((data) => {
-        alert(data['msg']);
-    })
-}
-
