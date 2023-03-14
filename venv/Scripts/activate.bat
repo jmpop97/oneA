89d1ee3c3a81586138a461e151f6a6e259249e69
Lee Juneyoung <wnsdud318@gmail.com>
2023-03-14 21:17:40 +0900

    button, array etc update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jakep\OneDrive\바탕 화면\camp\oneA\venv
+set VIRTUAL_ENV=C:\Users\Administrator\Desktop\sparta\teamprojects\02.mini\OneA\oneA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
