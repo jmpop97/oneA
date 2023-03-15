94821086671294ad26804b512e52362fac20fd05
jmpop <pjm970128@gmail.com>
2023-03-15 19:27:11 +0900

    수정 ㅈㅅ

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jeeni\Documents\GitHub\oneA\venv
+set VIRTUAL_ENV=C:\Users\jakep\Documents\GitHub\oneA\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
